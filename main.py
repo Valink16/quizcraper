4d5fd35b85897d56040bfd98d1856517b38d6ce2
Valink16 <valuthyk@gmail.com>
2019-05-22 19:46:35 +0200

    Improved get_terms

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
+import logging
 import quizcraper
+
+logging.basicConfig(level=logging.INFO)
 
 driver = webdriver.Firefox(executable_path="./geckodriver")
 quiz_url = "https://quizlet.com/170585467/44-a-48-flash-cards/"
 
-quizcraper.get_terms(driver, quiz_url)
+print(quizcraper.get_terms(driver, quiz_url, 0, 10))
--- quizcraper.py
+++ quizcraper.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
 def get_terms(web_driver, quiz_url, start = 0, end = -1):
     """
-        returns a list of terms which go from number<start> to number<end>
+        returns a list tuples of terms which go from number<start> to number<end>
     """
+    term_class_name = "SetPageTerms-term" # Class name of "terms"
+    # xpaths relative to the webelements with the <term_class_name> class
+    first_term_relative_xpath = "div/div/div[1]/div/div[1]/div/a/span"
+    second_term_relative_xpath = "div/div/div[1]/div/div[2]/div/a/span"
     web_driver.get(quiz_url)
-    terms = web_driver.find_elements_by_xpath("//span[starts-with(@class,'TermText')]")
-    print("Found {} non-translated terms...".format(len(terms)))
-    for term in terms:
-        print(term.text)
+
+    terms = web_driver.find_elements_by_class_name(term_class_name)
+    logging.info("Found {} terms".format(len(terms)))
+    if end == -1:
+        end = len(terms) - 1
+    
+    return [(t.find_element_by_xpath(first_term_relative_xpath).text, t.find_element_by_xpath(second_term_relative_xpath).text) for t in terms[start:end]]
